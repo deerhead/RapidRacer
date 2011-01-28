dde06521eed14bf70ffe6be7a04f4925a07e8b9a
deerhead <DavidJoel@gmx.ch>
2011-01-29 00:51:29 +0100

    Changed style, began to comment link_finder

--- src/RapidRacer/url_token_defines.py
+++ src/RapidRacer/url_token_defines.py
@@ -4,44 +4,48 @@ AND = "&"
 REGEXP_URL = r"[\w,\-,+,\\,/,&,=,.]*"
 
 #### FilesTube ####
-FT_URL                  = "http://www.filestube.com/"
+FT_URL = "http://www.filestube.com/"
 
-FT_SEARCH               = "search.html?"
-FT_SEARCH_RAPIDSHARE    = "1"
-FT_SEARCH_MEGAUPLOAD    = "3"
-FT_SEARCH_HOTFILE       = "27"
+FT_SEARCH = "search.html?"
+FT_SEARCH_RAPIDSHARE = "1"
+FT_SEARCH_MEGAUPLOAD = "3"
+FT_SEARCH_HOTFILE = "27"
 
-FT_SEARCH_PAGE          = lambda page: "page="+str(page)
+FT_SEARCH_PAGE = lambda page: "page=" + str(page)
+
 
 def FT_SEARCH_HOSTS(host_list):
-    
+
     host_var = "hosting="
     for host in host_list:
-        host_var = host_var+host+","
+        host_var = host_var + host + ","
     return host_var[0:-1]
+
 
 def FT_SEARCH_KEYWORDS(keyword_list):
     
     search_var = "q="
     for keyword in keyword_list:
-        search_var = search_var+keyword+"+"
+        search_var = search_var + keyword + "+"
     return search_var[0:-1]
 ###################
 
 #### Rapidshare API ####
-RS_API_URL          = "http://api.rapidshare.com/cgi-bin/rsapi.cgi?"
-RS_API_CHECK_FILES  = "sub=checkfiles&files=" 
-RS_API_ADD_FILE     = lambda file_id, filename: file_id+"&"+"filenames="+filename
+RS_API_URL = "http://api.rapidshare.com/cgi-bin/rsapi.cgi?"
+RS_API_CHECK_FILES = "sub=checkfiles&files=" 
+RS_API_ADD_FILE = lambda file_id, filename: file_id + "&" + "filenames=" + filename
 ########################
 
 #### Google Search ####
-GOOGLE_URL         = "http://www.google.com/search?"
-GOOGLE_SEARCH_PAGE  = lambda page_nr: "start="+page_nr
+GOOGLE_URL = "http://www.google.com/search?"
+GOOGLE_SEARCH_PAGE = lambda page_nr: "start=" + page_nr
+
 
 def GOOGLE_SEARCH_KEYS(keyword_list):
     
     search_var = "q="
     for keyword in keyword_list:
-        search_var = search_var+keyword+"+"
+        search_var = search_var + keyword + "+"
     return search_var[0:-1]
 #######################
+
--- src/test.py
+++ src/test.py
@@ -2,23 +2,34 @@ from RapidRacer import get_rs_file_info, link_finder
 from RapidRacer.url_token_defines import *
 
 if __name__ == "__main__":
-	file1 = get_rs_file_info.RSFile("http://rapidshare.com/files/120453782/2002")
-	print file1.get_status()
-	file1.reset_url("http://rapidshare.com/files/305232527/BatyasSeele.part1.rar.html")
-	print file1.get_status()
-	
-	search1 = link_finder.FilesTubeSearch(["Linkin","Park"],
-										 [FT_SEARCH_RAPIDSHARE])
-	print search1.get_link_list()
-	print search1.get_rs_link_list()
-	search1.debug_get_url()
-	
-	for url in search1.get_link_list():
-		if not "filestube" in url:
-			continue
-		try:
-			print url
-			search2 = link_finder.Finder(url)
-			print search2.get_rs_link_list()
-		except:
-			continue
+    file1 = get_rs_file_info.RSFile("http://rapidshare.com/files/120453782/2002")
+    print file1.get_status()
+    file1.reset_url("http://rapidshare.com/files/305232527/BatyasSeele.part1.rar.html")
+    print file1.get_status()
+    
+    ### Doesnt work, don't know why... ###
+    search3 = link_finder.GoogleSearch(["the","acacia","strain","rapidshare"])
+    print search3.get_link_list()
+    for url in search3.get_link_list():
+        try:
+            print url
+            search4 = link_finder.Finder(url)
+            print search4.get_rs_link_list()
+        except:
+            continue
+           
+#    search1 = link_finder.FilesTubeSearch(["the", "acacia", "strain"],
+#                                         [FT_SEARCH_RAPIDSHARE])
+#    print search1.get_link_list()
+#    print search1.get_rs_link_list()
+#    search1.debug_get_url()
+
+#    for url in search1.get_link_list():
+#        if not "filestube" in url:
+#            continue
+#        try:
+#            print url
+#            search2 = link_finder.Finder(url)
+#            print search2.get_rs_link_list()
+#        except:
+#            continue
