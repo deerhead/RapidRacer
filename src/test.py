5f6effc9eaf2bed387ec0118536421f3b0c6de1a
tux <tux@DavidJoel@gmx.ch>
2011-01-30 19:12:28 +0100

    first commit from ws

--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@ from RapidRacer import get_rs_file_info, link_finder
 from RapidRacer.url_token_defines import *
 
 if __name__ == "__main__":
+
     file1 = get_rs_file_info.RSFile("http://rapidshare.com/files/120453782/2002")
     print file1.get_status()
     file1.reset_url("http://rapidshare.com/files/305232527/BatyasSeele.part1.rar.html")
@@ -9,12 +10,14 @@ if __name__ == "__main__":
     
     search3 = link_finder.FilesTubeSearch(["the","acacia","strain"],
                                           [FT_SEARCH_RAPIDSHARE])
-        
+
     for u in range(10):
 
         search3.set_page_nr(u)    
         search3.reload_page()
         for url in search3.get_link_list():
+            if not "filestube" in url:
+                continue
             try:
                 print url
                 search4 = link_finder.Finder(url)
