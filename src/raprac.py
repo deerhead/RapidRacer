51c88749a7f6787dcbb2d57cd353b4cdd8761c5c
David Herrmann <tav@nest.(none)>
2011-02-20 12:27:02 -1000

    redesigned rapidracer

--- src/raprac.py
+++ src/raprac.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
 from optparse import OptionParser
-from RapidRacer.get_rs_file_info import RSFile
-from RapidRacer.link_finder import *
-from RapidRacer.url_token_defines import *
+from RapidRacer import *
 import threading
 from time import sleep
 
 def check_for_rs_links(link):
-    
+
     # Search for rapidshare links, add them to the link
     # dictionary and returns '2' if there are enough links
     # collected.
@@ -18,16 +16,22 @@ def check_for_rs_links(link):
         if options.debug:
             print "[*] Error while checking",link
         return 1
-    
-    if      options.host == "rapidshare":            
+
+    if      (options.host == "rapidshare" or
+             options.host == "rs"):
         link_list   = finder.get_rs_link_list()
-    elif    options.host == "megaupload":
+    elif    (options.host == "megaupload" or
+             options.host == "mu"):
         link_list   = finder.get_mu_link_list()
-    elif    options.host == "hotfile":
+    elif    (options.host == "hotfile" or
+             options.host == "hf"):
         link_list   = finder.get_hf_link_list()
-    elif    options.host == "uploaded":
+    elif    (options.host == "uploaded" or
+             options.host == "uploaded"):
         link_list   = finder.get_ul_link_list()
-    
+    else:
+        print "ELSE!"
+
     if len(link_list):
         link_dict.update({link:link_list})
         if options.debug:
@@ -36,40 +40,43 @@ def check_for_rs_links(link):
                 print "\t'-> "+ln
     if len(link_dict) == int(options.count):
         return 2
-    
+
 class SearchAtFilesTube(threading.Thread):
-    
+
     def __init__(self):
-        
+
         threading.Thread.__init__(self)
-        
+
         self.__checked_links = []
-        
+
         try:
-            
-            self.__ftsearch = FilesTubeSearch(args,[options.host],[options.filetype])
+            self.__ftsearch = FilesTubeSearch(args,[options.host],
+                                              [options.filetype])
         # If the given one-click-hoster isn't known, the link_finder
         # will raise a HostError exception
         except HostError,host:
             print "The given host %s is not a known one-click-hoster"%host
             exit(2)
-                    
+        except FiletypeError, filetype:
+            print "The given Filetype %s is not a known filetype"%filetype
+            exit(3)
+
     def run(self):
-        
+
         global link_dict
-            
+
         self.__nr=1
         while True:
             for link in self.__ftsearch.get_link_list():
 
                 if not "filestube" in link:
                     continue
-                # Manage and check the self.__checked_links-list    
+                # Manage and check the self.__checked_links-list
                 if link in self.__checked_links:
                     continue
                 self.__checked_links.append(link)
-                
-                # 
+
+                #
                 do = check_for_rs_links(link)
                 if do == 1:
                     continue
@@ -78,34 +85,35 @@ class SearchAtFilesTube(threading.Thread):
             # If there are less than 150 links at the site, return
             if len(self.__ftsearch.get_link_list())<150:
                 return
-            # Load the next page from the filestube search    
+            # Load the next page from the filestube search
             self.__nr+=1
             self.__ftsearch.set_page_nr(self.__nr)
             self.__ftsearch.reload_page()
-            
-            
+
+
 class SearchAtGoogle(threading.Thread):
-    
+
     def __init__(self):
-        
+
         threading.Thread.__init__(self)
-        
+
         self.__checked_links = []
         try:
-            
-            self.__gsearch = GoogleSearch(args,[options.host],[options.filetype])
-            
+
+            self.__gsearch = GoogleSearch(args,[options.host],
+                                          [options.filetype])
+
         # If the given one-click-hoster isn't known, the link_finder
         # will raise a HostError exception
         except HostError,host:
             print "The given host %s is not a known one-click-hoster"%host
-            
+
         self.__checked_links = []
-        
+
     def run(self):
-        
+
         global link_dict
-            
+
         self.__nr=1
         while True:
             for link in self.__gsearch.get_link_list():
@@ -113,7 +121,7 @@ class SearchAtGoogle(threading.Thread):
                 if link in self.__checked_links:
                     continue
                 self.__checked_links.append(link)
-                
+
                 do = check_for_rs_links(link)
                 if do == 1:
                     continue
@@ -129,7 +137,7 @@ class SearchAtGoogle(threading.Thread):
 
 
 if __name__ == "__main__":
-    
+
     ### Set up the OptionParser
     opt_parser = OptionParser("raprac [options] search keywords")
     opt_parser.add_option("-g", "--google", dest="google",
@@ -142,33 +150,34 @@ if __name__ == "__main__":
                           help="Debugging mode", action="store_true",
                           default=False)
     opt_parser.add_option("-c", "--count", dest="count")
-    opt_parser.add_option("-t", "--filetype", dest="filetype")    
+    opt_parser.add_option("-t", "--filetype", dest="filetype")
     opt_parser.add_option("-H", "--host", dest="host")
 
     (options, args) = opt_parser.parse_args()
     ###
-    
+
     # Check the given parameters and arguments
     if len(args)<1:
         opt_parser.error("No keywords given.")
-        
+
     if not options.google and not options.filestube:
-        opt_parser.error("No search parameter given.")
-        
+        opt_parser.error("No search machine defined")
+
     if not options.count:
         options.count = 1
-     
-    # Set a search for rapidshare links and all file type as default   
+
+    # Set a search for rapidshare links and all file type as default
     if not options.host:
-        options.host = "rs"
-    
+        options.host = "rapidshare"
+
     if not options.filetype:
         options.filetype = "all"
-    
+
     # Declaration of some important dictionaries
     link_dict   = {}
     thread_dict = {}
-    
+    link_list	= []
+
     # Run the filestube search if the parameter was set
     if options.filestube:
         thread_dict.update({"FilesTubeSearch":SearchAtFilesTube()})
@@ -181,9 +190,9 @@ if __name__ == "__main__":
         thread_dict["GoogleSearch"].start()
         if options.debug:
             print "[*] Searching at Google"
-        
+
     print "Racing..."
-    
+
     # Wait until the threads have finished
     for key in thread_dict.keys():
         thread_dict[key].join()
